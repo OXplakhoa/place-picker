7089db68846934ae39773c9f991b86bf000c326f
OXplakhoa <OXplakhoa@gmail.com>
2025-01-16 12:46:14 +0700

    Split Progress to avoid unnecessary computaion

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,29 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgessBar";
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   const TIMER = 3000;
-  const [remainTime,setRemainTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log("INTERVAL")
-      setRemainTime(prev => prev - 10);
-    },10)
-    return () => {
-      clearInterval(interval);
-    }
-  },[])
-
   useEffect(() => {
     console.log("TIMER SET");
     const timer = setTimeout(() => {
       onConfirm();
-    },TIMER);
+    }, TIMER);
     return () => {
-      console.log("Clear Timer")
-      clearTimeout(timer)
-    }
-  },[onConfirm])
+      console.log("Clear Timer");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -36,7 +25,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainTime} max={TIMER}/>
+      <ProgressBar TIMER={TIMER}/>
     </div>
   );
 }
